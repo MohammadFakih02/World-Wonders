7497709c5c2bf94228b2c2c8dc39cb0632d1962f
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-11-02 13:12:26 +0200

    added wonder details to detail page

--- Scripts/detail.js
+++ Scripts/detail.js
@@ -1,1 +1,48 @@
-const wondername= new URLSearchParams(window.location.search).get('name');
+let wondername= new URLSearchParams(window.location.search).get('name');
+wondername=wondername.toLowerCase();
+const instance = axios.create({
+    baseURL: "https://www.world-wonders-api.org/v0/wonders/name",
+  });
+
+if(wondername){
+    instance.get(`/${encodeURIComponent(wondername)}`)
+    .then(Response=>{
+        const wonder =Response.data;
+        const container= document.getElementById('wondercontainer');
+
+        const name = document.createElement('h1');
+        name.textContent=wonder.name;
+        container.appendChild(name);
+        
+        const imagecontainer = document.createElement('div');
+        imagecontainer.classList.add('image-container')
+        wonder.links.images.forEach(element => {
+            let image= document.createElement('img');
+            image.src=element;
+            container.append(image);
+        });
+
+        const summary = document.createElement('p');
+        summary.textContent=wonder.summary;
+        container.appendChild(summary);
+
+        const location=document.createElement('p');
+        location.textContent=`Location : ${wonder.location}`;
+        container.appendChild(location);
+
+        const buildyear=document.createElement('p');
+        buildyear.textContent=`Build Year: ${wonder.buildyear}`;
+        container.appendChild(buildyear);
+
+        const linksContainer = document.createElement('div');
+            linksContainer.classList.add('links');
+            linksContainer.innerHTML = `
+                <strong>Links:</strong>
+                <a href="${wonder.links.wiki}" target="_blank">Wikipedia</a>
+                <a href="${wonder.links.britannica}" target="_blank">Britannica</a>
+                <a href="${wonder.links.google_maps}" target="_blank">Google Maps</a>
+                <a href="${wonder.links.trip_advisor}" target="_blank">TripAdvisor</a>
+            `;
+            container.appendChild(linksContainer);
+    })
+}
--- Scripts/index.js
+++ Scripts/index.js
@@ -14,7 +14,7 @@ axios.get('https://www.world-wonders-api.org/v0/wonders/')
             name.textContent = wonder.name;
             wonderCard.appendChild(name);
             
-            if (wonder.links.images && wonder.links.images.length > 0) {
+            if (wonder.links.images) {
                 const image = document.createElement('img');
                 image.src = wonder.links.images[0];
                 image.style.width = '100%';
